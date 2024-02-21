9dc8a2a74e9de525b56b1bee5d6ab9828d9cd1bf
Andrew <thum14252@gmail.com>
2024-02-21 19:56:14 +0630

    adjust y random button

--- function.js
+++ function.js
@@ -5,7 +5,7 @@ function yes() {
 
 function moveBtn() {
     let x = Math.random() * (390 - document.getElementById("noBtn").offsetWidth);
-    let y = Math.random() * (800 - document.getElementById("noBtn").offsetHeight);
+    let y = Math.random() * (500 - document.getElementById("noBtn").offsetHeight);
     document.getElementById("noBtn").style.position = "absolute";
     document.getElementById("noBtn").style.left = `${x}px`;
     document.getElementById("noBtn").style.top = `${y}px`;
